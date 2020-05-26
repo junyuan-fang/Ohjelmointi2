7c27d92a99166399b49004af094efb4a810d3716
Junyuan Fang <Junyuan.fang@tuni.fi>
2020-05-26 15:39:00 +0300

    Syotteen lukeminen ja numeroiden tulostus

---
+++ student/02/number_series_game/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int input;
+    cout << "How many numbers would you like to have? ";
+    cin >>input;
+    for (int i=1; i< input+1;i++){
+        cout<<i<<endl;
+    }
+
+
+}
