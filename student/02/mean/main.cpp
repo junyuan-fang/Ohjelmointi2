be7cba536dd29ac87ad855d69bd3237fe074a51d
Junyuan Fang <Junyuan.fang@tuni.fi>
2020-05-26 21:28:32 +0300

    (H) Keskiarvo

--- student/02/mean/main.cpp
+++ student/02/mean/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
-
+using namespace std;
 // Write here a function counting the mean value
 
 int main()
 {
-    std::cout << "From how many integer numbers you want to count the mean value? ";
+    int kertoja = 0;
+    float sum= 0;
+
+    cout << "From how many integer numbers you want to count the mean value? ";
+    cin >> kertoja;
+
+    if (kertoja>0){
+        int i = 0;
+        while (i<kertoja){
+            int input_value;
+            cout<<"Input "<<i+1<<". number: ";
+            cin>>input_value;
+            sum+=input_value;
+            i++;
+        }
+        float keskiarvo= sum/kertoja;
+        cout<<"Mean value of the given numbers is "<<keskiarvo<<endl;
+
+    }else {
+        cout<<"Cannot count mean value from "<< kertoja <<" numbers"<<endl;
+        }
 }
+
+
