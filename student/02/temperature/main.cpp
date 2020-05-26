afc2c3b7cd7423f8a69d34a9eb67a432c149a9d6
Junyuan Fang <Junyuan.fang@tuni.fi>
2020-05-26 11:55:18 +0300

    (H) Lampotiloja

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    int temp;
     cout << "Enter a temperature: ";
-
-    // Write your code here
-
+    cin>>temp;
+    float fahrenheit= temp*1.8+32;
+    float celsius=(temp-32)/1.8;
+    cout << temp<<" degrees Celsius is "<<fahrenheit<<" degrees Fahrenheit"<<endl;
+    cout << temp<<" degrees Farenheit is "<<celsius<<" degrees Celsius"<<endl;
     return 0;
 }
