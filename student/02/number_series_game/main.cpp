d75f40216e94c47094758b5bb553f7ed21dd696b
Junyuan Fang <Junyuan.fang@tuni.fi>
2020-05-26 15:43:00 +0300

    Lisatty 3:lla jaollisten lukujen kasittely

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -6,7 +6,13 @@ int main()
     cout << "How many numbers would you like to have? ";
     cin >>input;
     for (int i=1; i< input+1;i++){
-        cout<<i<<endl;
+        if (i%3==0){
+            cout<<"zip"<<endl;
+        }
+        else{
+          cout<<i<<endl;
+        }
+
     }
 
 
