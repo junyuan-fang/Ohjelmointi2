464267f25bafaf474c86cb84bb22a46e6b142c46
Junyuan Fang <Junyuan.fang@tuni.fi>
2020-05-26 16:05:49 +0300

    ?Lisatty myos 7:lla jaollisten lukujen kasittely?

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -6,9 +6,19 @@ int main()
     cout << "How many numbers would you like to have? ";
     cin >>input;
     for (int i=1; i< input+1;i++){
-        if (i%3==0){
+        if ((i%3==0)&&(i%7==0)){
+            cout<<"zip boing"<<endl;
+        }
+        else if (i%3==0){
             cout<<"zip"<<endl;
         }
+
+        else if (i%7==0){
+            cout<<"boing"<<endl;
+        }
+
+
+
         else{
           cout<<i<<endl;
         }
