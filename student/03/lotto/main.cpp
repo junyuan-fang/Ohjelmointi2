965ec40edbaaec2e8656a28af734a2c85a97946b
Junyuan Fang <Junyuan.fang@tuni.fi>
2020-05-28 12:07:38 +0300

    Lottopeli tehty. Tietotyyppi unsigned_long_int  pystyy tallentaa  arvoa 20n arvoon asti.

--- student/03/lotto/main.cpp
+++ student/03/lotto/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+unsigned long int  kertoma(int luku);
+
+//kayttaa kertoman funtkiota, ja muodostaa kombinaatio.
+unsigned long int kombinaatio(int pallo, int drawn){
+    unsigned long int kombinaatio=1;
+
+    kombinaatio=kertoma(pallo)/(kertoma(pallo-drawn)*kertoma(drawn));
+    return kombinaatio;
+}
+
+//laske kertomia kombinaatiota varten
+unsigned long int kertoma(int luku){
+    //for (int tulos=1; luku>0; --luku)
+    unsigned long int tulos=1;
+    while(luku>0){
+        tulos=tulos*luku;
+        --luku;
+        //cout<<tulos<<endl;
+    }
+    //cout<<endl;
+    return tulos;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int pallo;
+    cout << "Enter the total number of lottery balls: ";
+    cin >>pallo;
+
+    int drawn;
+    cout<<"Enter the number of drawn balls: ";
+    cin>>drawn;
+
+    if ((pallo<0)||(drawn<0)){
+        cout<<"The number of balls must be a positive number."<<endl;
+    }
+
+    else{
+        cout<<"The probability of guessing all "
+           <<drawn<<" balls correctly is 1/"<<kombinaatio(pallo, drawn)<<endl;
+    }
     return 0;
 }
