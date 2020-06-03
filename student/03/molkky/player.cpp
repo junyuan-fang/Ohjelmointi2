79a7710bc99d51a25c97841c97d844b38f74e55e
Junyuan Fang <Junyuan.fang@tuni.fi>
2020-06-03 22:32:49 +0300

    all uploaded

--- student/03/molkky/player.cpp
+++ student/03/molkky/player.cpp
@@ -1,6 +1,33 @@
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
 #include "player.hh"
 
-Player::Player()
-{
+Player::Player(string name):
+name_(name),pts_(0){
+}
 
+string Player::get_name() const{
+    return name_;
+}
+
+int Player::get_points() const{
+    return pts_;
+}
+
+void Player::add_points(int pts){
+    pts_+=pts;
+}
+bool Player::has_won(){
+    if (pts_>50){
+        pts_=25;
+        return false;
+    }
+    else if (pts_==50){
+        return true;
+    }
+    else {
+        return false;
+    }
 }
--- student/03/molkky/player.hh
+++ student/03/molkky/player.hh
@@ -1,11 +1,20 @@
 #ifndef PLAYER_HH
-#define PLAYER_HH
-
+#include <string>
+// PLAYER_HH
+using namespace std;
 
 class Player
 {
 public:
-    Player();
+    Player(string name);
+    string get_name() const;
+    void add_points(int pts);
+    bool has_won();
+    int get_points() const;
+private:
+    string name_;
+    int pts_;
+
 };
 
 #endif // PLAYER_HH
