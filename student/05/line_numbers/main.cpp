b0c9fd961c67b7ed674312118447626415c9e85a
Junyuan Fang <Junyuan.fang@tuni.fi>
2020-06-15 15:26:40 +0300

    Ohjelma lukee tiedostoa ja muokkaa toisen tiedoston

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+void get_name( string* target){
+    cin>>* target;
+    string line_feed="";
+    getline(cin,line_feed);
+}
+
+
+int main()
+{
+    string in_file_name="";
+    cout<<"Input file: ";
+    get_name(&in_file_name);
+
+    string out_file_name="";
+    cout<<"Output file: ";
+    get_name(&out_file_name);
+
+    ifstream infile_var(in_file_name);
+    if (! infile_var){
+        cout<<"Error! The file not_a_file.input cannot be opened."<<endl;
+        return EXIT_FAILURE;
+    }
+    else{
+        ofstream outfile_var(out_file_name);
+        string rivi;
+        int i=1;
+        while(getline(cin,rivi)){
+            outfile_var<<i<<" "<<rivi<<endl;
+            ++i;
+        }
+        outfile_var.close;
+
+    }
+    infile_var.close()
+
+}
