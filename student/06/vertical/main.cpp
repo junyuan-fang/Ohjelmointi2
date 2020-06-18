c818563f095afc539fcdbcdf83d80a0580e6d27f
Junyuan Fang <Junyuan.fang@tuni.fi>
2020-06-19 01:01:20 +0300

    korjattu

--- student/06/vertical/main.cpp
+++ student/06/vertical/main.cpp
@@ -9,23 +9,12 @@ void print_vertical(unsigned int num)
 {
     RECURSIVE_FUNC
     // Do not remove RECURSIVE_FUNC declaration, it's necessary for automatic testing to work
-    string num_str=to_string(num);
-    if((num<=9)){
-        cout<<num<<endl;
+    if(num<=9){
+        std::cout<<num<<endl;
     }
     else{
-        cout<<num_str.at(0)<<endl;
-        unsigned int substruction=num_str.at(0)-48;
-        for(int i =1; i<(int)num_str.length();++i){
-            substruction*=10;
-        }
-        if((num-substruction)<=9){
-            cout<<"0"<<endl;
-//            for(int i=0;i<(int)num_str.length()-1;++i){
-//                cout<<"0"<<endl;
-//            }
-        }
-        return print_vertical(num-substruction);
+         print_vertical(num/10);
+         cout<<num%10<<endl;
     }
 
 
