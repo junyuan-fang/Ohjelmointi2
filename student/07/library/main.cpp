8f52d8ba8fc6ed737070ec8e2bda688be3387d23
Junyuan Fang <Junyuan.fang@tuni.fi>
2020-06-24 20:25:06 +0300

    korjaukset: 1.materiaali_tulostus()//tuntematon kirjaston tulostus on nyt oikein. 2.kirjojen_tulostus()//nyt tulostaa kirjan nimi, ja tulostu on nyt oikein 3.lainava_tulostus()//loyton jalkeen "tekija_loytetty"+1 4.kirjojen_tulostus()// tulostaan nainoastaan "one the shelf" 5.tiedosdon_lukeminen()//tulostaa virhetilanteet oikein automaattitesti 2

--- student/07/library/main.cpp
+++ student/07/library/main.cpp
@@ -47,7 +47,7 @@ std::vector<std::string> split(const std::string& s, const char delimiter, bool 
     return result;
 }
 
-string tiedosdon_lukeminen(const string& tiedostonimi,map<string,set<book>>&tallennetut_tiedot){
+void tiedosdon_lukeminen(const string& tiedostonimi,map<string,set<book>>&tallennetut_tiedot){
     ifstream tiedostomuuttuja(tiedostonimi);
     if(tiedostomuuttuja){
         string rivi;
@@ -56,7 +56,7 @@ string tiedosdon_lukeminen(const string& tiedostonimi,map<string,set<book>>&tall
             vector<string> parts=split(rivi,';');
             if (parts.size()!=4){//jos ei ole vaaditussa muodossa
                 cout<<"Error: empty field"<<endl;
-                return "FAILED";
+                exit(1);
             }
             else{
                 kirja.author=parts.at(1);
@@ -79,19 +79,20 @@ string tiedosdon_lukeminen(const string& tiedostonimi,map<string,set<book>>&tall
                 }
             }
        }
+        if(tiedostomuuttuja.eof()){
+            cout<<"Error: empty field"<<endl;
+        }
     }
     else{
         cout<<"Error: the input file cannot be opened"<<endl;
-        return "FAILED";
     }
-    return "SUCCESS";
 }
 // alkaa nama funktiot, jotka ovat kayttoliittymalle
 //ja nama funktiot ovat jarjestyksessa
 void materiaali_tulostus(const string& kirjasto_nimi,
                          const collected_data& tallennetut){
     if(tallennetut.find(kirjasto_nimi)==tallennetut.end()){
-        cout<<"Error: unknown "<< kirjasto_nimi<<" name"<<endl;
+        cout<<"Error: unknown library name"<<endl;
     }
     else{
         for( book kirja : tallennetut.at(kirjasto_nimi)){
@@ -112,10 +113,10 @@ void kirjojen_tulostus(const string& kirjasto_nimi,
         for( book kirja : kirjat){
             if(kirja.author==tekija){
                 if(kirja.reservations==1){
-                    cout<<kirja.author<<" --- one the shelf"<<endl;
+                    cout<<kirja.title<<" --- one the shelf"<<endl;
                 }
                 else{
-                    cout<<kirja.author<<" --- "<<kirja.reservations<<" reservations"<<endl;
+                    cout<<kirja.title<<" --- "<<kirja.reservations<<" reservations"<<endl;
                 }
                  ++tekija_loytetty;
             }
@@ -134,9 +135,11 @@ void reservable_printing(const string& kirjan_nimi, const collected_data& tallet
             if(book_struct.title==kirjan_nimi){
                 reservable+=book_struct.reservations;
                 kirjastot.insert(talle.first);
+                esiityminen++;
             }           
         }
     }
+    cout<<kirjan_nimi<<endl;
     if(esiityminen==0){//jos kirja on esiintynyt
         cout<<"Book is not a library book."<<endl;
         return;
@@ -160,7 +163,9 @@ void lainava_tulostus(const collected_data& tallennetut_tiedot){
         for(book kirja:talle_rivi.second){
             string tulostettava_rivi;
             tulostettava_rivi=kirja.author+": "+kirja.title;
-            tulostettava_set.insert(tulostettava_rivi);//tulostettavn set on tehty
+            if(kirja.reservations==1){//jos on the shelf
+                tulostettava_set.insert(tulostettava_rivi);//tulostettavn set on tehty
+            }
         }
     }
 
@@ -174,85 +179,77 @@ int main()
     cout<<"Input file: ";
     getline(cin,tiedostonimi);
     map<string,set<book>>tallennetut_tiedot;
+    tiedosdon_lukeminen(tiedostonimi,tallennetut_tiedot);
 
-    if(tiedosdon_lukeminen(tiedostonimi,tallennetut_tiedot)
-            =="FAILED"){
-        return EXIT_FAILURE;
-    }
-    else{//"SUCCESS"
-        while (true){
-            string order;
-            cout<<"> ";
-            getline(cin,order);
+    while (true){
+        string order;
+        cout<<"> ";
+        getline(cin,order);
 
-            vector<string> order_parts;
-            order_parts=split(order,' ',true);
+        vector<string> order_parts;
+        order_parts=split(order,' ',true);
 
-            if(order=="quit"){
-                return  EXIT_SUCCESS;
+        if(order=="quit"){
+            return  EXIT_SUCCESS;
+        }
+
+        else if(order_parts.at(0)=="libraries"){
+            map<string,set<book>>::iterator iter=tallennetut_tiedot.begin();
+            while(iter!=tallennetut_tiedot.end()){
+                cout<<iter->first<<endl;
+                ++iter;
+            }
+        }
+
+        else if(order_parts.at(0)=="material"){
+            if(order_parts.size()!=2){
+                cout<<"Error: error in command "<<order_parts.at(0)<<endl;
+            }
+            else{
+                string kirjasto_nimi=order_parts.at(1);
+                materiaali_tulostus(kirjasto_nimi,tallennetut_tiedot);
+            }
+        }
+        //books <kirjasto> <tekija>
+        else if(order_parts.at(0)=="books"){
+            if (order_parts.size()!=3){
+                cout<<"Error: error in command "<<order_parts.at(0)<<endl;
+            }
+            else{
+                string kirjasto_nimi=order_parts.at(1);
+                string tekija=order_parts.at(2);
+                kirjojen_tulostus(kirjasto_nimi,tekija, tallennetut_tiedot);
+
             }
 
-            else if(order_parts.at(0)=="libraries"){
-                map<string,set<book>>::iterator iter=tallennetut_tiedot.begin();
-                while(iter!=tallennetut_tiedot.end()){
-                    cout<<iter->first<<endl;
+        }
+        else if (order_parts.at(0)=="reservable"){
+            string kirjan_nimi;
+            if(order.find("\"")!=string::npos){
+                int index_alku = order.find("\"")+1;
+                int index_loppu = order.find_last_of("\"");
+                kirjan_nimi = order.substr(index_alku,index_loppu-index_alku);
+            }
+            else{
+                vector<string> ::iterator iter=++order_parts.begin();
+                while(iter!=order_parts.end()){
+                    if(iter!=order_parts.end()-1){
+                        kirjan_nimi+=(*iter+" ");
+                    }
+                    else{
+                        kirjan_nimi+=*iter;
+                    }
                     ++iter;
                 }
             }
-
-            else if(order_parts.at(0)=="material"){
-                if(order_parts.size()!=2){
-                    cout<<"Error: error in command "<<order_parts.at(0)<<endl;
-                }
-                else{
-                    string kirjasto_nimi=order_parts.at(1);
-                    materiaali_tulostus(kirjasto_nimi,tallennetut_tiedot);
-                }
-            }
-            //books <kirjasto> <tekija>
-            else if(order_parts.at(0)=="books"){
-                if (order_parts.size()!=3){
-                    cout<<"Error: error in command "<<order_parts.at(0)<<endl;
-                }
-                else{
-                    string kirjasto_nimi=order_parts.at(1);
-                    string tekija=order_parts.at(2);
-                    kirjojen_tulostus(kirjasto_nimi,tekija, tallennetut_tiedot);
-
-                }
-
-            }
-            else if (order_parts.at(0)=="reservable"){
-                string kirjan_nimi;
-                if(order.find("\"")!=string::npos){
-                    int index_alku = order.find("\"")+1;
-                    int index_loppu = order.find_last_of("\"");
-                    kirjan_nimi = order.substr(index_alku,index_loppu-index_alku);
-                }
-                else{
-                    vector<string> ::iterator iter=++order_parts.begin();
-                    while(iter!=order_parts.end()){
-                        if(iter!=order_parts.end()-1){
-                            kirjan_nimi+=(*iter+" ");
-                        }
-                        else{
-                            kirjan_nimi+=*iter;
-                        }
-                        ++iter;
-                    }
-                }
-                reservable_printing(kirjan_nimi,tallennetut_tiedot);
-            }
-            else if (order_parts.at(0)=="loanable"){
-                lainava_tulostus(tallennetut_tiedot);
-            }
-            else{
-                cout<<"Error: Unknown command: "<<order_parts.at(0)<<endl;
-            }
+            reservable_printing(kirjan_nimi,tallennetut_tiedot);
+        }
+        else if (order_parts.at(0)=="loanable"){
+            lainava_tulostus(tallennetut_tiedot);
+        }
+        else{
+            cout<<"Error: Unknown command: "<<order_parts.at(0)<<endl;
         }
     }
-
-    
-    
     return EXIT_SUCCESS;
 }
