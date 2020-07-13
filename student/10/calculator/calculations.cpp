6b5dc4e752f5c72faae0008d063481733026d2f5
Junyuan Fang <Junyuan.fang@tuni.fi>
2020-07-13 22:42:26 +0300

    Nelilaskimen toteutus funktion osoitimella, ja tietuella.

---
+++ student/10/calculator/calculations.cpp
@@ -0,0 +1,29 @@
+// Note that there is no need for "using namespace std",
+// since no C++ standard libraries are used.
+
+double addition(double left, double right) {
+    return left + right;
+}
+
+
+double subtraction(double left, double right) {
+    return left - right;
+}
+
+
+double multiplication(double left, double right) {
+    return left * right;
+}
+
+
+double division(double left, double right) {
+    return left / right;
+}
+
+double power(double left, double right){
+    double num=left;
+    for (int i=1; i<right; ++i){
+        num*=left;
+    }
+    return num;
+}
